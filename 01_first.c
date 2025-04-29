61807c58423d99236ccfa30da47877e84c1b0641
ABHINAY <abhinaykumarakula5@gmail.com>
2025-04-29 18:52:42 +0530

    Add files via upload

---
+++ 01_first.c
@@ -0,0 +1,14 @@
+// OM NAMO VENKATESHAYA
+// AUTHOR :ABHINAY
+// BASIC C PROGRAM
+
+#include <stdio.h> //preprocesssor directive
+
+int main()
+{ // main function - program starts from here
+
+    // displaying the output
+    printf("OM NAMO VENKATESHAYA \n");
+    return 0; // program ended successfully
+    // returns zero to os
+}
